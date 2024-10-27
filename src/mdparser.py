46dc07558b054e2940365791d13b8dcc4bf2e8fd
Kyle Cannoles <kylecann@hawaii.edu>
2024-10-27 21:20:45 +0900

    Feat: Add image and link parsing

--- src/mdparser.py
+++ src/mdparser.py
@@ -1,3 +1,5 @@
+import re
+
 from textnode import TextNode, TextType
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -39,3 +41,12 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         else:
             new_nodes.extend(node)
     return new_nodes
+
+def extract_markdown_images(text):
+    image_list = []
+    matches = re.findall(r"!\[([^\[\]]*?)\]\(([^\(\)]*?)\)", text)
+    return matches
+def extract_markdown_links(text):
+    image_list = []
+    matches = re.findall(r"(?:[^!]|^)\[([^\[\]]*?)\]\(([^\(\)]*?)\)", text)
+    return matches
--- src/test_mdparser.py
+++ src/test_mdparser.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-from mdparser import split_nodes_delimiter
+from mdparser import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 class TestInline(unittest.TestCase):
     def test_split_delimiter(self):
@@ -48,4 +48,20 @@ class TestInline(unittest.TestCase):
         node_two = TextNode("Node two with a `code block` word", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node_one, node_two], "`", TextType.CODE)
         self.assertEqual(new_nodes, [TextNode("Node one with a ", TextType.TEXT), TextNode("code block", TextType.CODE), TextNode(" word", TextType.TEXT), TextNode("Node two with a ", TextType.TEXT), TextNode("code block", TextType.CODE), TextNode(" word", TextType.TEXT),])
+
+    def test_extract_markdown_images(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        self.assertEqual([("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")], extract_markdown_images(text))
+    def test_extract_markdown_images_parse_error(self):
+        text = "This is text with a ![rick roll(https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        self.assertEqual([("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")], extract_markdown_images(text))
+    def test_extract_markdown_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        self.assertEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], extract_markdown_links(text))
+    def test_prevent_extract_md_images_as_links(self):
+        text = "![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        self.assertEqual([], extract_markdown_links(text))
+    def test_extract_md_links_first_char(self):
+        text = "[to boot dev](https://www.boot.dev) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        self.assertEqual([("to boot dev", "https://www.boot.dev")], extract_markdown_links(text))
 
