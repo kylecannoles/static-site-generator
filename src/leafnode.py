4444471f2544c428ea56fa15d3dee31f65a98eed
Kyle Cannoles <kylecann@hawaii.edu>
2024-10-27 11:35:54 +0900

    Fix: Make tag and value attributes required

--- src/leafnode.py
+++ src/leafnode.py
@@ -1,7 +1,7 @@
 from htmlnode import HTMLNode
 
 class LeafNode(HTMLNode):
-    def __init__(self, tag=None, value=None, props=None):
+    def __init__(self, tag, value, props=None):
         HTMLNode.__init__(self, tag, value, None, props)
     def to_html(self):
         if self.value == None:
