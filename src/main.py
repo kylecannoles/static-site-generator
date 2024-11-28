1de5a03eccff2164ff533f6b73bfc8f1ac671a9c
Kyle Cannoles <kylecann@hawaii.edu>
2024-11-28 15:11:24 +0900

    Add function to copy directory tree from static -> public

--- src/main.py
+++ src/main.py
@@ -1,7 +1,27 @@
+from os import mkdir, path, listdir
+from shutil import rmtree, copy
 from textnode import TextNode, TextType
 
 def main():
-    text_node = TextNode("This is a text node", TextType.BOLD, "https://google.com")
-    print(text_node)
+    copy_static_to_public()
+
+def copy_files(src,dest, file_list):
+    for file in file_list:
+        src_file_path = path.join(src,file)
+        dest_file_path = path.join(dest,file)
+        if path.isfile(src_file_path):
+            copy(src_file_path, dest_file_path)
+        else:
+            mkdir(dest_file_path)
+            copy_files(src_file_path, dest_file_path, listdir(src_file_path)) 
+
+def copy_static_to_public(src="static", dest="public"):
+    # Delete contents of public
+    if path.exists(dest):
+        rmtree(dest)
+    # Create public folder
+    mkdir("public")
+    file_list = listdir(src)
+    copy_files(src,dest, file_list)
 
 main()
